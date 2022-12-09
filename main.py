ddc1dfca1edc71c84059242e3683d80dcdf4042c
sarobson.carvalho <sarobson.carvalho@nellieanalytics.ie>
2022-12-09 14:40:07 +0000

    fixing some issues and add new properties

--- main.py
+++ main.py
@@ -17,25 +17,51 @@ state_list = parse_state_list("terraform show -json")
 
 state = json.loads(state_list)
 raw_resources = state["values"]["root_module"]["resources"]
+#print(state["values"]["root_module"]["child_modules"][1])
 
-#properties = {'azurerm_resource_group': 'id', 'azurerm_resource_group': 'name', 'azurerm_virtual_network':'name'}
+for res in state["values"]["root_module"]["child_modules"]:
+    #print(res["child_modules"])
+    raw_resources.extend(res["resources"])
+    if "child_modules" in res:
+        for b in res["child_modules"]:
+            if "resources" in b:
+                print("ok")
+                raw_resources.extend(b["resources"])
+#print(state["values"]["root_module"]["child_modules"][1]["child_modules"][0]["resources"])
+    
+    
+  #raw_resources.extend(res)
+  #print(res["resources"])
+  #if(res["resources"]["child_modules"]):
+    #print("tem")
+    #for res2 in res["resources"]["child_modules"]:
+         #raw_resources.extend(res2["resources"])
+         #print(res2["resources"])
+  
 
 properties = {
     "azurerm_resource_group": ["name","location"],
-    "azurerm_virtual_network": ["name","address_space","guid"]
+    "azurerm_virtual_network": ["name","address_space","dns_servers"],
+    "azurerm_subnet": ["name","address_prefixes","virtual_network_name"],
+    "azurerm_public_ip_prefix": ["name","sku","ip_prefix","prefix_length","zones"],
+    "azurerm_firewall_policy_rule_collection_group": ["name"],
+    "azurerm_virtual_network_peering": ["name","allow_forwarded_traffic","allow_gateway_transit","allow_virtual_network_access","use_remote_gateways"]
 }
 
 def extract_resource_values(resource):
     # Use regular expressions to extract the values for the attributes
     # that you are interested in from the state_info list
     resource_values = {}
+    #print(resource['type'])
     if resource['type'] in properties:
         resource_values['type'] = resource['type']
+       
         values = properties.get(resource['type'])
         for value in resource['values']:
             if value in values:
                 resource_values[value] = resource['values'][value]
-    return resource_values
+    if resource_values:
+        return resource_values
 
 
 
@@ -63,7 +89,7 @@ def create_resource_table(resource_type, resources):
         for property in resource_properties:
             # Convert the list of values to a string using an empty string as the separator
             property_values = resource[property]
-            property_value = "".join(property_values)
+            property_value = "".join(str(property_values))
             table_row.append(property_value)
             table_row.append("|")
         table += "".join(table_row) + "\n"
@@ -73,9 +99,11 @@ resources = []
 
 # Iterate over the list of resources and print the resource type
 for resource in raw_resources:
-  resources.append(extract_resource_values(resource))
+  item = extract_resource_values(resource)
+  if item:
+    resources.append(item)
 
-# # Group the resources by their type
+# Group the resources by their type
 resources_by_type = {}
 for resource in resources:
     if resource["type"] not in resources_by_type:
